3d02699e81f358018e4302591fb78042e201d172
Xia Wu <xiawu@newtonproject.org>
2019-04-03 00:11:12 +0800

    init project

---
+++ newstudio-debug/index.js
@@ -0,0 +1,39 @@
+'use strict'
+var EthDebugger = require('./src/Ethdebugger')
+var TransactionDebugger = require('./src/debugger/debugger')
+var CmdLine = require('./src/cmdline')
+
+var StorageViewer = require('./src/storage/storageViewer')
+var StorageResolver = require('./src/storage/storageResolver')
+
+var SolidityDecoder = require('./src/solidity-decoder')
+
+var remixLib = require('newstudio-lib')
+var BreakpointManager = remixLib.code.BreakpointManager
+
+/*
+  Use of breakPointManager :
+
+  var breakPointManager = new BreakpointManager(this.debugger, (sourceLocation) => {
+    return line/column from offset (sourceLocation)
+  })
+  this.debugger.setBreakpointManager(breakPointManager)
+*/
+module.exports = {
+  EthDebugger: EthDebugger,
+  TransactionDebugger: TransactionDebugger,
+  /**
+   * constructor
+   *
+   * @param {Object} _debugger - type of EthDebugger
+   * @return {Function} _locationToRowConverter - function implemented by editor which return a column/line position for a char source location
+   */
+  BreakpointManager: BreakpointManager,
+  SolidityDecoder: SolidityDecoder,
+  storage: {
+    StorageViewer: StorageViewer,
+    StorageResolver: StorageResolver
+  },
+  CmdLine: CmdLine
+}
+
